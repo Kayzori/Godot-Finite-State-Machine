f53b80835b3c3aeeec6518a0728da60860d45550
Kayzori <kayzori7@gmail.com>
2025-06-16 22:49:27 +0100

    Update FiniteStateMachinePlugin.gd

--- FiniteStateMachine/plugin/FiniteStateMachinePlugin.gd
+++ FiniteStateMachine/plugin/FiniteStateMachinePlugin.gd
@@ -2,7 +2,7 @@
 extends EditorPlugin
 
 func _enter_tree() -> void:
-    add_custom_type("FiniteStateMachine", "Node", preload("res://FiniteStateMachine/FiniteStateMachine.gd"), preload("res://FiniteStateMachine/plugin/theme/FiniteStateMachine.png"))
+    add_custom_type("FiniteStateMachine", "Node", preload("res://Addons/FiniteStateMachine/FiniteStateMachine.gd"), preload("res://Addons/FiniteStateMachine/plugin/theme/FiniteStateMachine.png"))
 
 func _exit_tree() -> void:
     remove_custom_type("StateMachine")
